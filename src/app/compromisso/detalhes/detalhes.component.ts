a7e5715c0ef57e0c9885841ab3724ae0b5398271
Munif Gebara Junior <munifgebara@gmail.com>
2018-12-26 13:07:32 -0200

    crud

--- src/app/compromisso/detalhes/detalhes.component.ts
+++ src/app/compromisso/detalhes/detalhes.component.ts
@@ -3,52 +3,17 @@ import { ServiceService } from '../service.service';
 import { DFResponse } from 'src/app/dfresponse.type';
 import { Compromisso } from '../compromisso.type';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { SuperDetalhes } from 'src/app/comum/super-detalhes';
 @Component({
   selector: 'app-detalhes',
   templateUrl: './detalhes.component.html',
   styleUrls: ['./detalhes.component.css']
 })
-export class DetalhesComponent implements OnInit {
+export class DetalhesComponent extends SuperDetalhes<Compromisso> implements OnInit {
 
-  protected selecionado: Compromisso = {};
-  protected id = "new";
-
-  constructor(protected service: ServiceService, protected router: Router, protected route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.id = this.route.snapshot.params.id;
-    if (this.id == "new") {
-      this.selecionado = this.service.newObject();
-      this.selecionado.id = "new";
-    }
-    else {
-      this.service.getOne(this.id).then(obj => {
-        this.selecionado = obj;
-      });
-    }
-
+  constructor(service: ServiceService, router: Router, route: ActivatedRoute) {
+    super(service, router, route);
   }
-
-  cancel() {
-    this.router.navigate(['../..'], { relativeTo: this.route });
-  }
-
-  save() {
-    if (this.id == "new") {
-      this.service.add({ ...this.selecionado, id: undefined }).then((r) => this.router.navigate(['../..'], { relativeTo: this.route }));
-    } else {
-      this.service.modify(this.selecionado).then((r) => this.router.navigate(['../..'], { relativeTo: this.route }));
-    }
-  }
-
-  delete() {
-    if (this.id == "new") {
-      this.router.navigate(['../..'], { relativeTo: this.route });
-    } else {
-      this.service.remove(this.id).then((r) => this.router.navigate(['../..'], { relativeTo: this.route }));
-    }
-  }
-
 }
 
 
---
+++ src/app/comum/super-detalhes.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { SuperService } from './super-service';
+import { DFResponse } from 'src/app/dfresponse.type';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+export class SuperDetalhes<Tipo> implements OnInit {
+
+    protected selecionado: Tipo;
+    protected id = "new";
+    protected service: SuperService<Tipo>;
+    protected router: Router;
+    protected route: ActivatedRoute;
+    constructor(service: SuperService<Tipo>, router: Router, route: ActivatedRoute) {
+        this.service = service;
+        this.router = router;
+        this.route = route;
+    }
+    ngOnInit() {
+        this.id = this.route.snapshot.params.id;
+        if (this.id == "new") {
+            this.selecionado = this.service.newObject();
+            this.selecionado['id'] = "new";
+        }
+        else {
+            this.service.getOne(this.id).then(obj => {
+                this.selecionado = obj;
+            });
+        }
+
+    }
+
+    cancel() {
+        this.router.navigate(['../..'], { relativeTo: this.route });
+    }
+
+    save() {
+        if (this.id == "new") {
+            this.selecionado['id'] = undefined;
+            this.service.add(this.selecionado).then((r) => this.router.navigate(['../..'], { relativeTo: this.route }));
+        } else {
+            this.service.modify(this.selecionado).then((r) => this.router.navigate(['../..'], { relativeTo: this.route }));
+        }
+    }
+
+    delete() {
+        if (this.id == "new") {
+            this.router.navigate(['../..'], { relativeTo: this.route });
+        } else {
+            this.service.remove(this.id).then((r) => this.router.navigate(['../..'], { relativeTo: this.route }));
+        }
+    }
+
+}
+
+
